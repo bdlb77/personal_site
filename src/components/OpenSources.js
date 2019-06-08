9fefbab84d47fc041446784eabcbb289479c64fd
Bryan Leighton <bryan.leigh77@gmail.com>
2019-06-08 11:35:28 +0300

    github api linked up

--- src/components/OpenSources.js
+++ src/components/OpenSources.js
@@ -1,12 +1,41 @@
 import React from "react";
 import ArticlesOS from "./styles/ArticlesOS";
+import { StaticQuery, graphql } from "gatsby";
 
-const OpenSources = props => (
-  <ArticlesOS>
-    <OpenSource />
-    <OpenSource />
-  </ArticlesOS>
+const OpenSources = ({ githubData }) => {
+  console.log(githubData);
+  return (
+    <ArticlesOS>
+      <OpenSource repo={githubData.data.repository} />
+    </ArticlesOS>
+  );
+};
+const OpenSource = ({ repo }) => (
+  <div>
+    <img src={repo.owner.avatarUrl} alt={repo.owner.login} />
+    <h1>{repo.owner.login}</h1>
+  </div>
 );
 
-const OpenSource = props => <div>Open Source</div>;
-export default OpenSources;
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query onGitHub {
+        githubData {
+          data {
+            repository {
+              owner {
+                login
+              }
+              pullRequest {
+                url
+                mergedAt
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={({ githubData }) => <OpenSources githubData={githubData} />}
+  />
+);
